c966da1ec12bfa0062138d7c4e149ece81b85bf8
qwwqe <f.werth@gmail.com>
2023-05-08 14:57:39 +0800

    Test: 粗略撰寫authenticate單元測試

---
+++ repos/hero/test/rest.ts
@@ -0,0 +1,141 @@
+import test from "node:test";
+import assert from "assert";
+import RestHeroRepo from "../rest";
+
+test("RestHeroRepo", async (t) => {
+  /**
+   * @todo 考慮簡化mock——畢竟我們要測試的純粹是authenticate
+   * 怎麼處理遠端的各種回傳，而不是有沒有正確地建構請求物件吧。
+   */
+  await t.test("authenticate", async (t) => {
+    const fetcher: typeof fetch = async (_, init) => {
+      if (!init?.body) {
+        return new Response("", { status: 500 });
+      }
+
+      const o = JSON.parse(init.body.toString());
+
+      if (
+        !("name" in o) ||
+        !("password" in o) ||
+        Object.getOwnPropertyNames(o).length !== 2
+      ) {
+        return new Response("無效格式", { status: 400 });
+      }
+
+      if (o.name !== "hahow" || o.password !== "rocks") {
+        return new Response("驗證失敗", { status: 401 });
+      }
+
+      return new Response("驗證成功", { status: 200 });
+    };
+
+    await t.test("successful with correct credentials", async () => {
+      const repo = new RestHeroRepo({
+        host: "https://hahow-recruit.herokuapp.com",
+        port: "443",
+        fetcher,
+      });
+
+      const r = await repo.authenticate({
+        name: "hahow",
+        password: "rocks",
+      });
+
+      assert.ok(r.ok);
+    });
+
+    await t.test("unsuccessful with invalid credentials", async () => {
+      const repo = new RestHeroRepo({
+        host: "https://hahow-recruit.herokuapp.com",
+        port: "443",
+        fetcher,
+      });
+
+      const r = await repo.authenticate({
+        name: "zazow",
+        password: "zockz",
+      });
+
+      assert.ok(!r.ok);
+    });
+
+    await t.test("correct error code for invalid credentials", async () => {
+      const repo = new RestHeroRepo({
+        host: "https://hahow-recruit.herokuapp.com",
+        port: "443",
+        fetcher,
+      });
+
+      const r = await repo.authenticate({
+        name: "zazo",
+        password: "zockz",
+      });
+
+      assert.ok(!r.ok);
+      assert.equal(r.code, "err.repo.auth");
+    });
+
+    await t.test("unsuccessful with invalid request format", async () => {
+      const repo = new RestHeroRepo({
+        host: "https://hahow-recruit.herokuapp.com",
+        port: "443",
+        fetcher,
+      });
+
+      const a: any = repo.authenticate.bind(repo);
+      const r: Awaited<ReturnType<typeof repo.authenticate>> = await a({
+        show: "me the secret",
+      });
+
+      assert.ok(!r.ok);
+    });
+
+    await t.test("correct error code for invalid request format", async () => {
+      const repo = new RestHeroRepo({
+        host: "https://hahow-recruit.herokuapp.com",
+        port: "443",
+        fetcher,
+      });
+
+      const a: any = repo.authenticate.bind(repo);
+      const r: Awaited<ReturnType<typeof repo.authenticate>> = await a({
+        show: "me the secret",
+      });
+
+      assert.ok(!r.ok);
+      assert.equal(r.code, "err.repo.client");
+    });
+
+    await t.test("unsuccessful with unknown response", async () => {
+      const repo = new RestHeroRepo({
+        host: "https://hahow-recruit.herokuapp.com",
+        port: "443",
+        fetcher: async () => new Response("", { status: 500 }),
+      });
+
+      const r = await repo.authenticate({
+        name: "hahow",
+        password: "rocks",
+      });
+
+      assert.ok(!r.ok);
+    });
+
+    await t.test("correct error code for unknown response", async () => {
+      const repo = new RestHeroRepo({
+        host: "https://hahow-recruit.herokuapp.com",
+        port: "443",
+        fetcher: async () => new Response("", { status: 500 }),
+      });
+
+      const r = await repo.authenticate({
+        name: "hahow",
+        password: "rocks",
+      });
+
+      assert.ok(!r.ok);
+      assert.equal(r.code, "err.repo.unknown");
+    });
+  });
+});
