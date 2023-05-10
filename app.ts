92a298667ff7544d4851e1ae820a8b538eeca0e1
qwwqe <f.werth@gmail.com>
2023-05-11 01:06:45 +0800

    Feature: 將Hero路由納入app

--- app.ts
+++ app.ts
@@ -1,8 +1,16 @@
 import Koa from "koa";
 import HeroRouter from "@routes/hero";
+import RestHeroRepo from "@repos/hero/rest";
 
 const koa = new Koa();
 
-koa.use(HeroRouter.routes());
+const heroRepo = new RestHeroRepo({
+  host: "https://hahow-recruit.herokuapp.com",
+  port: "443",
+});
+
+const heroRouter = HeroRouter(heroRepo);
+
+koa.use(heroRouter.routes());
 
 export default koa;
