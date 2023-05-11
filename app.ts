0ccf59f198c50cb2bc07865bcfcdeb0f03e069fb
qwwqe <f.werth@gmail.com>
2023-05-11 15:47:50 +0800

    Feature: 納入全域錯誤處理器

--- app.ts
+++ app.ts
@@ -1,8 +1,22 @@
 import Koa from "koa";
 import HeroRouter from "@routes/hero";
 import RestHeroRepo from "@repos/hero/rest";
+import ErrorResponse from "@errors";
 
 const koa = new Koa();
+
+const globalErrorHandler: Parameters<typeof koa.use>[0] = async (ctx, next) => {
+  try {
+    await next();
+  } catch (err) {
+    ctx.body = {
+      code: "err.global.unknown",
+      message: "未知錯誤",
+    } as ErrorResponse;
+  }
+};
+
+koa.use(globalErrorHandler);
 
 const heroRepo = new RestHeroRepo({
   host: "https://hahow-recruit.herokuapp.com",
